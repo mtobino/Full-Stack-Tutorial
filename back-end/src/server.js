1ddaa2deb5f64399a50bb90a8fe2369ccaf8cf38
Matthew Tobino <tobino68@students.rowan.edu>
2024-07-20 23:19:10 -0400

    Added starting backend files and reoganized

---
+++ back-end/src/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+
+let articlesInfo = [{
+    name:'learn-react',
+    upvotes:0,
+    comments:[]
+},{
+    name:'learn-node',
+    upvotes:0,
+    comments:[]
+},{
+    name:'mongo',
+    upvotes:0,
+    comments:[]
+}]
+
+
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if(article){
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvptes`);
+    }
+    else{
+        res.send("The article does not exist");
+    }
+
+})
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {postedBy, text} = req.body;
+    const { name } = req.params;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if(article){
+        article.comments.push({ postedBy, text })
+        res.send(article.comments)
+    }
+    else{
+        res.send("The article does not exist");
+    }
+})
+
+
+
+app.listen(8000, () => console.log('Listening on port 8000'));
